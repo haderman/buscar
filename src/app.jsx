0da194152d8e38a5d51240cd85321fb5d4c1df4e
haderman <hadermania@gmail.com>
2024-03-24 11:22:10 -0500

    Optimize CSS loading and clean up project structure
    - Add preload for CSS in index.html to improve loading performance.
    - Remove unused CSS rules from src/index.css to streamline styling.
    - Clean up commented code and imports in src/app.jsx.
    - Remove unnecessary whitespace in vite.config.js for cleaner code formatting.

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,6 @@ import PropTypes from 'prop-types'
 
 import { Layout } from './components/layout'
 
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
-import './index.css'
-
 App.propTypes = {
   children: PropTypes.node,
 }
